2522ab56962f2edb3a3c4fa41f1bf25679217635
Pierre Mavro <pmavro@4tech.io>
2018-03-05 00:28:36 +0100

    feat: setting v0.4 version

--- cmd/root.go
+++ cmd/root.go
@@ -22,7 +22,7 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version number of msa-deployer",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("v0.3")
+		fmt.Println("v0.4")
 	},
 }
 
